57676314333aa6dd0e8ec7bff956d28052f80f2b
김준표juunpy0 <junpyokim0106@gmail.com>
2026-07-26 13:41:37 +0900

    🍎-chore :: [#1] 프로젝트 초기 설정

---
+++ Moil/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moil
+//
+//  Created by 김준표 on 7/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moil/MoilApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoilApp.swift
+//  Moil
+//
+//  Created by 김준표 on 7/26/26.
+//
+
+import SwiftUI
+
+@main
+struct MoilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
